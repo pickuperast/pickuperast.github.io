2add9d486b0332cac16dc727c741d865e3aa565b
pickuperast <s.seidgaparov@gmail.com>
2020-02-14 18:15:42 +0600

    new build

--- StreamingAssets/oilan.js
+++ StreamingAssets/oilan.js
@@ -1,138 +1,18 @@
-function testVoid( ) {
+function testVoid() {
+    var num=parseInt(value);
+    unityInstance.SendMessage('WebGLMessageHandler', 'RecieveMessageInt',num);
     window.alert('Void');
+   console.log(num);
+   console.log(typeof (num));
 }
-
-function testInt(intVal) {
-    window.alert('Int');
-}
-
-function testString(stringVal) {
-    window.alert('String');
-}
-
-function testReturnInt( ) {
-    return 123;
-}
-
-function testReturnString( ) {
-    return 'This is String';
-}
-
 
 function GetUserID(){
-	
-	console.log("GetUserID");
-	
-	return 0;
+	var val = parseInt(value);
+	console.log("UserID: "+val);
+	return val;
 }
 
-
-function GetStarsTotal(userID){
-	
-	console.log("GetStarsTotal");
-	
-	return 0;
-}
-
-
-function SetStarsTotal(userID, value){
-	
-	console.log("SetStarsTotal");
-	
-}
-
-
-function GetStarsCollectedInLocationStep(userID, level, step){
-	
-	console.log("GetStarsCollectedInLocationStep");
-	
-	return 0;
-}
-
-
-function GetStarsCollectedInLocationPart(userID, level, step, part){
-	
-	console.log("GetStarsCollectedInLocationPart");
-	
-	return 0;
-}
-
-
-function SetStarsCollectedInLocationStep(userID, level, step, value){
-	
-	console.log("SetStarsCollectedInLocationStep");
-	
-}
-
-function SetStarsCollectedInLocationPart(userID, level, step, part, value){
-	
-	console.log("SetStarsCollectedInLocationPart");
-	
-}
-
-function GetLocationIsUnlocked(userID, level, step){
-	
-	console.log("GetLocationIsUnlocked");
-	
-	return false;
-}
-
-function SetLocationIsUnlocked(userID, level, step, value){
-	
-	console.log("SetLocationIsUnlocked");
-	
-}
-
-function GetMaxUnlockedStepInLevel(userID, level){
-	
-	console.log("GetMaxUnlockedStepInLevel");
-	
-	return 0;
-}
-
-function GetLocationMax(userID){
-	
-	console.log("GetLocationMax");
-	
-	return "JSON_HERE";
-}
-
-function GetLocationLast(userID){
-	
-	console.log("GetLocationLast");
-	
-	return "JSON_HERE";
-}
-
-function GetLocationLastLevel(userID){
-	
-	console.log("GetLocationLastLevel");
-	
-	return 0;
-}
-
-function GetLocationLastStep(userID){
-	
-	console.log("GetLocationLastStep");
-	
-	return 0;
-}
-
-function GetLocationLastPart(userID){
-	
-	console.log("GetLocationLastPart");
-	
-	return 0;
-}
-
-function SetLocationLast(userID, level, step, part){
-	
-	console.log("SetLocationLast");
-	
-}
-
-function ShowTrainer(userID, level, step, type){
-	
-	console.log("ShowTrainer");
-	
+function GetProgress(){
+	console.log(progres);
+	return progres;
 }
