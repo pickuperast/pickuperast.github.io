98281e092f34cd6dfb4bfa7ec62e70b842b2b24c
Sanat Seidgaparov <s.seidgaparov@gmail.com>
2020-03-13 14:32:12 +0500

    test

--- StreamingAssets/oilan.js
+++ StreamingAssets/oilan.js
@@ -23,6 +23,15 @@ function SetProgress(JSONinput){
 	var l_progress = JSONinput;
 }
 
+function OpenTrainer(StringTrainerType, intLevel, intStep){
+	console.log("Trying to open Trainer: " + StringTrainerType + ", level: " + intLevel + ", step: " + intStep);
+	//put here code to perform trainer window opening
+}
+
 function ConsoleWriter(strInput){
 	console.log(strInput);
+}
+
+function printErr(strInput){
+	console.log("Error: "+ strInput);
 }
